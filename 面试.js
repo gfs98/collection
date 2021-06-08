3f21c096b8acff4412bcc3a4513af825fbd17f76
gfs98 <47024458+gfs98@users.noreply.github.com>
2021-06-08 15:18:07 +0800

    Update 面试.js

--- 面试.js
+++ 面试.js
@@ -1,13 +1,13 @@
 let a={
-				b:(()=>{
-					return [0,1,2].map((el)=>{
-						return {
-							el
-						}
-					}).some(el=>{
-						return !!el[Object.keys(el)[0]]
-					})
-				})()
+	b:(()=>{
+		return [0,1,2].map((el)=>{
+			return {
+				el
 			}
-			let b = Object.prototype.toString.call(a.b)
-			// console.log(b)
+		}).some(el=>{
+			return !!el[Object.keys(el)[0]]
+		})
+	})()
+}
+let b = Object.prototype.toString.call(a.b)
+// console.log(b)
